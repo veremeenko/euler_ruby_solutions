93aa5953c1543a37e2da56fde56d577a6a411e9b
Alex <euler@alex-VirtualBox.(none)>
2012-08-02 14:14:47 +0300

    Change tabs to spaces

--- spec/2_spec.rb
+++ spec/2_spec.rb
@@ -2,8 +2,8 @@ require "2"
 
 describe Do_2, "Problem 2" do
   it "By considering the terms in the Fibonacci sequence
-   		whose values do not exceed four million, f
-   		ind the sum of the even-valued terms."  do
+   	whose values do not exceed four million, f
+   	ind the sum of the even-valued terms."  do
     Do_2.new.go().should eq(4613732)
 
   end
